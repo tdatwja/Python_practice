8357c3ecc32edfb4b25f934ec8e8666fbd7a41f0
misaki <99859593+tdatwja@users.noreply.github.com>
2022-08-04 07:39:34 +0900

    Add files via upload

---
+++ Python_practice.py
@@ -0,0 +1,26 @@
+print("Hello,world!")
+
+print(4 + 5)
+print(8 - 1)
+print(2 * 7)
+print(6 / 2)
+print(9 % 2)
+
+name = "misaki"
+print(name)
+number = 2022
+print(number)
+
+onion_price = 80
+onion_count = 5
+total_price = onion_price * onion_count
+print(total_price)
+
+a = 90
+print(a)
+a *= 2
+print(a)
+
+print("Hello" + "My name is" + "misaki")
+day = "today"
+print(day + "is hot")
