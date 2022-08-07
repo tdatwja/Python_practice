41ae2595c9ac9de86b3570c79bcebc948352aea7
misaki <99859593+tdatwja@users.noreply.github.com>
2022-08-07 16:48:35 +0900

    Add files via upload

---
+++ Python_cyclohexane.py
@@ -0,0 +1,8 @@
+from turtle import *
+shape("turtle")
+LineColor = ["limegreen", "blue", "gold", "limegreen", "blue", "gold"]
+for i in range(6):
+    color(LineColor[i])
+    forward(150)
+    left(60)
+done()
